c9b5f6bb8d3eb3cf149887fe5209c4c25d4061ce
Qiujie Li <liqiujie2008@gmail.com>
2020-10-23 21:51:48 +1300

    Updates

---
+++ precache-manifest.456d753ba05494ea38dddb0bf3c7e660.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6641d9e351d8978ddbeda93ff99405a1",
+    "url": "/never-todo/index.html"
+  },
+  {
+    "revision": "ec952a8fee923304742f",
+    "url": "/never-todo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "62b5c06a3d58527e630e",
+    "url": "/never-todo/static/js/2.752bff90.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/never-todo/static/js/2.752bff90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec952a8fee923304742f",
+    "url": "/never-todo/static/js/main.f5bab3b6.chunk.js"
+  },
+  {
+    "revision": "d59acbbd874422109ecf",
+    "url": "/never-todo/static/js/runtime-main.6aeea707.js"
+  }
+]);
